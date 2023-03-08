9a6f1a0b0969473d56a3c95b0b15bceea9c6c38d
vincenttommi <vincenttommi@gmail.com>
2023-03-08 13:05:18 +0300

    invoking function in js

---
+++ invokingfunctions.js
@@ -0,0 +1,11 @@
+
+// The code inside a JavaScript function will execute when "something" invokes it.calls it
+
+function myfunction(a,b){
+
+    return a+b;
+
+}
+
+document.getElementById("demo").innerHTML = myfunction(30,40);
+
