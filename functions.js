0e9cd29ab09d923c5134fcb1df4786b3f361bc66
vincenttommi <vincenttommi@gmail.com>
2023-03-08 12:25:56 +0300

    convert from Fahrenheit to Celsius

--- functions.js
+++ functions.js
@@ -1,9 +1,22 @@
   
-  function  calculate(p1,p2) {
+  // function  calculate(p1,p2) {
 
-    return p1*p2;
+  //   return p1*p2;
+
+  // }
+
+  // document.getElementById("demo").innerHTML = calculate(4,3);
+
+
+  // This example calls a function to convert from Fahrenheit to Celsius
+
+
+  function toCelsius(f){
+   return (5/9) * (f-32);
+
 
   }
 
-  document.getElementById("demo").innerHTML = calculate(4,3);
 
+
+  document.getElementById("demo").innerHTML = toCelsius(77);
