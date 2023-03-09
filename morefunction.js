3fa5cc70bd6d8be052edfd40120ecb90652db8e1
vincenttommi <vincenttommi@gmail.com>
2023-03-09 06:52:44 +0300

    passing argument in a function

--- morefunction.js
+++ morefunction.js
@@ -1,11 +1,24 @@
-function greet() {
+// // 
 
-    console.log("hello")
+
+// function dosomething(thing){
+
+//     console.log(thing);
+
+// }
+
+// dosomething("anything");  // passing the argument anything' into our function
+
+
+
+function sayhelloTo(firstname){
+
+console.log(`Hello ${firstname}!`);
+
+
 }
+sayhelloTo("vincent");
+sayhelloTo("tommi");
+sayhelloTo(1);
 
- greet();
-
- console.log(1);
-
-
- greet();
+// In the example above, you may have noticed that there's also something inside the parentheses in the function declaration: function sayHelloTo(firstName). This is called the parameter, in this case firstName. A parameter is a placeholder that stores whatever value gets passed in as an argument. For example, when we run the function call sayHelloTo('Isabel'), the value of the argument, 'Isabel', gets stored in the parameter firstName. Then, inside the function body, we access that value by using the parameter name, interpolate it into the string, and log the string to the terminal.
