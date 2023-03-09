a289eb9e16f0eb2ca5aaeb64a2c7676f6544ff23
vincenttommi <vincenttommi@gmail.com>
2023-03-09 13:59:33 +0300

    finding html elements by id

---
+++ dom.js
@@ -0,0 +1,15 @@
+// HTML DOM methods are actions you can perform (on HTML Elements)
+// A property is a value that you can get or set (like changing the content of an HTML element).
+
+// A method is an action you can do (like add or deleting an HTML element).
+
+
+
+
+
+// document.getElementById("demo").innerHTML = "vincenttommi";
+
+
+// finding tags using dom
+
+document.getElementsByTagName("").innerHTML ="brayo";
--- morefunction.js
+++ morefunction.js
@@ -1,32 +1,42 @@
-// // 
+// // // 
 
 
-// function dosomething(thing){
+// // function dosomething(thing){
 
-//     console.log(thing);
+// //     console.log(thing);
+
+// // }
+
+// // dosomething("anything");  // passing the argument anything' into our function
+
+
+
+// function sayhelloTo(firstname){
+
+// console.log(`Hello ${firstname}!`);
+
 
 // }
+// sayhelloTo("vincent");
+// sayhelloTo("tommi");
+// sayhelloTo(1);
 
-// dosomething("anything");  // passing the argument anything' into our function
+// // In the example above, you may have noticed that there's also something inside the parentheses in the function declaration: function sayHelloTo(firstName). This is called the parameter, in this case firstName. A parameter is a placeholder that stores whatever value gets passed in as an argument. For example, when we run the function call sayHelloTo('Isabel'), the value of the argument, 'Isabel', gets stored in the parameter firstName. Then, inside the function body, we access that value by using the parameter name, interpolate it into the string, and log the string to the terminal.
+// function say(greeting, firstName){
+//     console.log(`${greeting},${firstName}`);
+//     console.log("firstName: ", firstName);
+//     console.log("greeting: ", greeting);
+//     console.log(`${greeting}, ${firstName}!`);
+// }
+
+// say("morning", "vincent");
 
 
+// adding using function
 
-function sayhelloTo(firstname){
-
-console.log(`Hello ${firstname}!`);
-
+function add(a,b){
+return a+b;
 
 }
-sayhelloTo("vincent");
-sayhelloTo("tommi");
-sayhelloTo(1);
 
-// In the example above, you may have noticed that there's also something inside the parentheses in the function declaration: function sayHelloTo(firstName). This is called the parameter, in this case firstName. A parameter is a placeholder that stores whatever value gets passed in as an argument. For example, when we run the function call sayHelloTo('Isabel'), the value of the argument, 'Isabel', gets stored in the parameter firstName. Then, inside the function body, we access that value by using the parameter name, interpolate it into the string, and log the string to the terminal.
-function say(greeting, firstName){
-    console.log(`${greeting},${firstName}`);
-    console.log("firstName: ", firstName);
-    console.log("greeting: ", greeting);
-    console.log(`${greeting}, ${firstName}!`);
-}
-
-say("morning", "vincent");
+console.log(add(2,4));
